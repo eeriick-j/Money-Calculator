3157ff246a6b5f2a95b2677e9a1d625e70c25fd7
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-11 17:59:47 +0100

    Added loader for all currencies

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,11 +1,14 @@
 package app;
 
+import io.CurrencyLoader;
 import models.Currency;
-import models.Money;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Money money = new Money(new Currency("EUR", "Euro"), 1);
-        System.out.println(money);
+    public static void main(String[] args) throws IOException {
+        List<Currency> currencyLoader = new CurrencyLoader().loadAll();
+        for (Currency currency : currencyLoader) System.out.println(currency);
     }
 }
---
+++ src/main/java/io/CurrencyLoader.java
@@ -0,0 +1,46 @@
+package io;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import models.Currency;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CurrencyLoader {
+    private static final String API_KEY = "78f2ad1a2e76eb7ca408357a";
+
+    private URLConnection openConnection() throws IOException {
+        String API_URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/";
+        return new URL(API_URL + "codes").openConnection();
+    }
+
+    private InputStream openInputStream(URLConnection connection) throws IOException {
+        return connection.getInputStream();
+    }
+
+    private JsonObject getJson(InputStream inputStream) throws IOException {
+        return new Gson().fromJson(new String(inputStream.readAllBytes()), JsonObject.class);
+    }
+
+    private List<Currency> jsonArrayToList(JsonArray jsonArray) {
+        List<Currency> list = new ArrayList<>();
+        for (JsonElement element : jsonArray) {
+            JsonArray tuple = element.getAsJsonArray();
+            list.add(new Currency(tuple.get(0).getAsString(), tuple.get(1).getAsString()));
+        }
+        return list;
+    }
+
+    public List<Currency> loadAll() throws IOException {
+        try (InputStream inputStream = openInputStream(openConnection())) {
+            return jsonArrayToList(getJson(inputStream).getAsJsonArray("supported_codes"));
+        }
+    }
+}
